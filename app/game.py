7da82e13d9d9a9960cee2d6bfc22df32ca722657
Joe Burgess <josephburgess@gmail.com>
2023-03-17 15:35:32 +0000

    first test for game class - adding frame to scorecard

---
+++ app/game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self):
+        self.scorecard = []
+
+    def add(self, frame):
+        self.scorecard.append(frame)
---
+++ tests/test_game.py
@@ -0,0 +1,19 @@
+import pytest
+from app.game import Game
+from unittest.mock import Mock
+
+
+open_frame = Mock(
+    get_first=lambda: 3,
+    get_second=lambda: 4,
+    get_third=lambda: 0,
+    get_total=lambda: 7,
+    is_strike=lambda: False,
+    is_spare=lambda: False,
+)
+
+
+def test_add_frame_to_scorecard():
+    game = Game()
+    game.add(open_frame)
+    assert game.scorecard == [open_frame]
