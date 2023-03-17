9588d1ea9bbd36dff34677a93a5c35826ad08ad1
Joe Burgess <josephburgess@gmail.com>
2023-03-17 13:42:29 +0000

    first test for frame class

---
+++ app/frame.py
@@ -0,0 +1,8 @@
+class Frame:
+    def __init__(self, first, second, third):
+        self.first = first
+        self.second = second
+        self.third = third
+
+    def get_first(self):
+        return self.first
--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,2 +1,10 @@
-def test_example():
-    assert 1 + 1 == 2
+import pytest
+from app.frame import Frame
+
+frame = Frame(10, 3, 4)
+
+
+def test_frame_rolls():
+    assert frame.first == 10
+    assert frame.second == 3
+    assert frame.third == 4
